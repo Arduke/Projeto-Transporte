89f9e1420916d2f3b59c06696ba5be60a6f0d445
Carlos Eduardo <Carlos.edumart@outlook.com>
2018-10-18 21:21:43 -0300

    Add files via upload

---
+++ ProjetoTransporte/Controler/Passageiros.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTransporte
+{
+    class Passageiros
+    {
+        #region Atributos
+        private int proximoId;
+        private Queue<Passageiro> filaPassageiros;
+        #endregion
+
+
+        #region propriedades
+        public int ProximoId
+        {
+            get
+            {
+                return proximoId;
+            }
+
+            set
+            {
+                proximoId = value;
+            }
+        }
+        #endregion
+
+        #region proteção
+        public Queue<Passageiro> Filapassageiros
+        {
+            get { return filaPassageiros; }
+        }
+
+        
+        #endregion
+
+        #region construtor
+        public Passageiros()
+        {
+            filaPassageiros = new Queue<Passageiro>();
+            this.proximoId = 1;
+        }
+        #endregion
+
+        #region metodo
+        public void gerar(String nome)
+        {
+            Filapassageiros.Enqueue(new Passageiro(nome,proximoId));
+            proximoId++;
+        }
+        #endregion
+    }
+}
---
+++ ProjetoTransporte/Model/Passageiro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTransporte
+{
+    class Passageiro
+    {
+
+        #region atribultos
+        private string nome;
+        private int codigo;
+        #endregion
+
+        #region construtor
+        public Passageiro(string nome, int codigo)
+        {
+            this.nome = nome;
+            this.codigo = codigo;
+        }
+        #endregion
+
+        #region propriedades
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value;
+            }
+        }
+        #endregion
+
+        #region metodos
+        public String dadosCompletos()
+        {
+            return nome + "-" + codigo;
+        }
+        #endregion
+
+    }
+}
